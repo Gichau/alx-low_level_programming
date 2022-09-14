4c7313ee979c2421a0fd290f6fe72ff58e2e3c39
Gichau <sirmeawesome@gmail.com>
2022-09-14 10:03:46 -0700

    time tabke

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,7 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			int prod = j * i;
-			{
+
 				if (j == 0)
 				{
 					_putchar('0');
@@ -32,7 +32,6 @@ void times_table(void)
 					_putchar(prod % 10 + '0');
 				}
 			}
-			_putchar('\n');
+		_putchar('\n');
 		}
 	}
-}
