561f0bccf81ce5b0c6deb0d355f66a7b1306d2f7
Gichau <sirmeawesome@gmail.com>
2022-09-15 05:19:47 -0700

    int mul(int a, int b);

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * _isdigit - checks if int c is a digit 0 through 9
- * @c: interger
+ * _isdigit - checks if int c is a digit 0 - 9
+ * @c: integer
  * Returns 1 if c is a digit and 0 if otherwise
  */
 int _isdigit(int c)
